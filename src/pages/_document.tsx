464dc28ec2078118c43b59e15b15458ba64979c7
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-17 22:06:42 -0300

    Add layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,24 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+/* https://nextjs.org/docs/advanced-features/custom-document */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import { GetStaticProps } from 'next';
+import Header from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi'
+import Link from "next/link";
 
+import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../services/prismic';
+
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+
 
 interface Post {
   uid?: string;
@@ -24,9 +30,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home() {
+  return (
+    <>
+    <main className={commonStyles.container} >
+    <Header />
+
+      <div className={styles.posts}>
+        <Link href="/">
+        <a className={styles.post}>
+          <strong> Algum titulo</strong>
+          <p>Pensando no desafio</p>
+          <ul>
+            <li>
+              <FiCalendar />
+              17 agosto 2021
+            </li>
+            <li>
+              <FiUser />
+              Joseane Guedes
+            </li>
+          </ul>
+        </a>
+        </Link>
+
+        <Link href="/">
+        <a className={styles.post}>
+          <strong> Outro titulo</strong>
+          <p>Pensando no desafio</p>
+          <ul>
+            <li>
+              <FiCalendar />
+              17 agosto 2021
+            </li>
+            <li>
+              <FiUser />
+              Joseane Guedes
+            </li>
+          </ul>
+        </a>
+        </Link>
+        <button type="button">
+          Carregar mais posts
+        </button>
+      </div>
+    </main>
+    </>
+  )
+ }
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
